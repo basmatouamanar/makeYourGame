9e79f475a162282b851a0f8944ce9c9f1f053d88
btoumana <basmatoumanar55@gmail.com>
2026-01-01 10:42:04 +0100

    update in ball

--- ball.js
+++ ball.js
@@ -1,4 +1,28 @@
 
+//let body = document.querySelector("body")
+let a = document.getElementById("pabble")
+let b = document.getElementById("board")
+//let x = 0
+let bHeight = b.offsetHeight
+let bdWidth = b.offsetWidth
+let aHeight = a.offsetHeight
+let aWidth = a.offsetWidth
+let pabbleY = bHeight - 50
+let pabbleX = bdWidth / 2 - (a.offsetWidth) / 2
+a.style.transform = `translate(${pabbleX}px, ${pabbleY}px)`
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+        pabbleX = pabbleX + 50
+        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
+        if (pabbleX > bdWidth - a.offsetWidth) pabbleX = bdWidth - a.offsetWidth
+    } else if (e.key === "ArrowLeft") {
+        pabbleX = pabbleX - 50
+        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
+        if (pabbleX < 0) pabbleX = 0
+        
+    }
+})
+
 let element = document.getElementById("balle")
 let board = document.getElementById("board")
 let boardWidth = board.offsetWidth
@@ -16,7 +40,14 @@ function ball() {
     if (x <= 0 || (x + letterWidth) >= boardWidth) {
         dx = -dx 
     }
+    if ((y + letterHeight) >= boardHeight) {
+        y = boardHeight - letterHeight
+        return 
+    }
     if (y <= 0 || (y + letterHeight) >= boardHeight) {
+        dy = -dy 
+    }
+    if ((x <= (pabbleX + aWidth) && x >= pabbleX) && (y >= aHeight+pabbleY))  {
         dy = -dy 
     }
     element.style.transform = `translate(${x}px, ${y}px)`
@@ -25,28 +56,6 @@ function ball() {
 }
 requestAnimationFrame(ball)
 
-
-//let body = document.querySelector("body")
-let a = document.getElementById("pabble")
-let b = document.getElementById("board")
-//let x = 0
-let bHeight = b.offsetHeight
-let bdWidth = b.offsetWidth
-let pabbleY = bHeight - 50
-let pabbleX = bdWidth / 2 - (a.offsetWidth) / 2
-a.style.transform = `translate(${pabbleX}px, ${pabbleY}px)`
-document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowRight") {
-        pabbleX = pabbleX + 30
-        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
-        if (pabbleX > bdWidth - a.offsetWidth) pabbleX = bdWidth - a.offsetWidth
-    } else if (e.key === "ArrowLeft") {
-        pabbleX = pabbleX - 30
-        a.style.transform = `translate(${pabbleX}px,  ${pabbleY}px)`
-        if (pabbleX < 0) pabbleX = 0
-
-    }
-})
 
 
 /*
